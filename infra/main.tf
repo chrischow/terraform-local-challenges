b7e7b94a96aed9e2aab1f50e9cf0f24224b6b778
chrischow <christian.chow.en.yi@gmail.com>
2026-06-11 09:36:29 +0800

    feat: challenge 05

--- infra/main.tf
+++ infra/main.tf
@@ -14,3 +14,13 @@ resource "local_file" "foo" {
   filename = var.filename
   content  = var.content
 }
+
+output "filename" {
+  description = "The filename of the local file"
+  value       = local_file.foo.filename
+}
+
+output "content" {
+  description = "The content of the local file"
+  value       = local_file.foo.content
+}
